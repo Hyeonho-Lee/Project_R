7c2e38f6fce0b16afb5020fbbcacdeb449c1362d
Hyeonho-Lee <prohenho7138@gmail.com>
2021-05-12 23:29:12 +0900

    update files

---
+++ Camera/TPSCamera.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TPSCamera : MonoBehaviour {
+    [Header("Camera Status")]
+    public Transform Player_Transform;
+    public Transform Camera_Arm;
+
+    private float mouse_horizontal;
+    private float mouse_vertical;
+
+    public Vector3 camera_dir;
+    public Vector3 player_dir;
+    public Vector3 camera_angle;
+
+    void Update() {
+        Look_Movement();
+    }
+
+    void Look_Movement() {
+        mouse_horizontal = Input.GetAxis("Mouse X");
+        mouse_vertical = Input.GetAxis("Mouse Y");
+
+        camera_angle = Camera_Arm.rotation.eulerAngles;
+        float x = camera_angle.x - mouse_vertical;
+
+        if (x < 180f)
+            x = Mathf.Clamp(x, -0.5f, 50f);
+        else
+            x = Mathf.Clamp(x, 335f, 361f);
+
+        Camera_Arm.rotation = Quaternion.Euler(x, camera_angle.y + mouse_horizontal, camera_angle.z);
+
+        camera_dir = Player_Transform.transform.position - this.transform.position;
+        player_dir.Set(camera_dir.x, 0, camera_dir.z);
+        Debug.DrawRay(Player_Transform.transform.position, player_dir * 0.3f, Color.red);
+    }
+}
--- movement/PlayerMovement.cs
+++ movement/PlayerMovement.cs
@@ -6,31 +6,38 @@ public class PlayerMovement : MonoBehaviour {
     [Header("PlayerMovement Status")]
     public float player_speed;
     public float player_jump;
+    public float rotation_speed;
 
     private float horizontal;
     private float vertical;
     public float ground_height;
 
+    [Header("PlayerMovement Check")]
     public bool is_jump;
     public bool is_ground;
 
-    Vector3 movement;
+    public Vector3 movement;
+    public Vector3 player_dir;
 
     Rigidbody rigidbody;
+    TPSCamera tpscamera;
 
     void Awake() {
         rigidbody = GetComponent<Rigidbody>();
+        tpscamera = GameObject.Find("TPS_Camera").GetComponent<TPSCamera>();
     }
 
     void Start() {
-        player_speed = 5f;
-        player_jump = 10f;
+        player_speed = 10.0f;
+        player_jump = 7.0f;
         ground_height = 1.75f;
+        rotation_speed = 10.0f;
     }
 
     void Update() {
         horizontal = Input.GetAxisRaw("Horizontal");
         vertical = Input.GetAxisRaw("Vertical");
+        player_dir = tpscamera.player_dir.normalized;
 
         Input_Key();
         Check_Value();
@@ -55,9 +62,54 @@ public class PlayerMovement : MonoBehaviour {
 
     void Move(float h, float v) {
         movement.Set(h, 0, v);
-        movement = movement.normalized * player_speed * Time.deltaTime;
+        movement = movement.normalized;
 
-        rigidbody.MovePosition(this.transform.position + movement);
+        Vector3 dir_forward = Quaternion.Euler(0f, 0f, 0f) * player_dir;
+        Vector3 dir_f_right = Quaternion.Euler(0f, 45f, 0f) * player_dir;
+        Vector3 dir_right = Quaternion.Euler(0f, 90f, 0f) * player_dir;
+        Vector3 dir_b_right = Quaternion.Euler(0f, 135f, 0f) * player_dir;
+        Vector3 dir_back = Quaternion.Euler(0f, 180f, 0f) * player_dir;
+        Vector3 dir_b_left = Quaternion.Euler(0f, 225f, 0f) * player_dir;
+        Vector3 dir_left = Quaternion.Euler(0f, 270f, 0f) * player_dir;
+        Vector3 dir_f_left = Quaternion.Euler(0f, 315f, 0f) * player_dir;
+
+        // 앞오
+        if ((movement.z > 0 && movement.z <= 1) && (movement.x > 0 && movement.x <= 1))
+            Move_Vector(dir_f_right);
+
+        // 앞왼
+        if ((movement.z > 0 && movement.z <= 1) && (movement.x < 0 && movement.x >= -1))
+            Move_Vector(dir_f_left);
+
+        // 뒤왼
+        if ((movement.z < 0 && movement.z >= -1) && (movement.x < 0 && movement.x >= -1))
+            Move_Vector(dir_b_left);
+
+        // 뒤오
+        if ((movement.z < 0 && movement.z >= -1) && (movement.x > 0 && movement.x <= 1))
+            Move_Vector(dir_b_right);
+
+        // 앞
+        if ((movement.z > 0 && movement.z <= 1) && (movement.x == 0))
+            Move_Vector(dir_forward);
+
+        // 뒤
+        if ((movement.z < 0 && movement.z >= -1) && (movement.x == 0))
+            Move_Vector(dir_back);
+
+        // 오
+        if ((movement.x > 0 && movement.x <= 1) && (movement.z == 0))
+            Move_Vector(dir_right);
+
+        // 왼
+        if ((movement.x < 0 && movement.x >= -1) && (movement.z == 0))
+            Move_Vector(dir_left);
+    }
+
+    void Move_Vector(Vector3 input_vector) {
+        Quaternion newRotation = Quaternion.LookRotation(input_vector);
+        this.transform.rotation = Quaternion.Slerp(this.transform.rotation, newRotation, rotation_speed * Time.deltaTime);
+        rigidbody.MovePosition(this.transform.position + input_vector * player_speed * Time.deltaTime);
     }
 
     void Jump(float speed) {
