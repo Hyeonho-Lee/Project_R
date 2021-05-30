cae23ccd79a89a9e4a5e7ba1b612b71021bbcb60
Hyeonho-Lee <prohenho7138@gmail.com>
2021-05-30 18:47:46 +0900

    update files

---
+++ Physics/BallisticsPhysics.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallisticsPhysics : MonoBehaviour {
+    Rigidbody rigidbody;
+    public float speed = 10f;
+
+    void Start() {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    
+    void FixedUpdate()
+    {
+    }
+}
